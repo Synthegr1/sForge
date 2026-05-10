37f39484fea5d1e8f5bf3aa483220377b74b3a6a
gab92240 <synthe.gr1@gmail.com>
2026-05-10 21:02:39 +0200

    Enhance data collection and section detection
    Added data collection fields and improved section detection logic.

--- Main.java
+++ Main.java
@@ -18,8 +18,21 @@ public class Main {
     static String project_name;
 	static String cat_name;
 	
+	//  --Collected Data--   //
+	// Project Data
 	static String arch;
 	static String out_name;
+	
+	//Objects Data
+	static String[] c_objects_name;
+	static String[] asm_objects_name;
+	static String[] rust_objects_name;
+	static String[] ld_objects_name;
+	
+	static String[] c_objects_path;
+	static String[] asm_objects_path;
+	static String[] rust_objects_path;
+	static String[] ld_objects_path;
    
     static File in = new File("main.txt");
     
@@ -73,6 +86,44 @@ public class Main {
     	}catch (FileNotFoundException e) {
 			System.out.println(RED + "File Not Found !" + RESET);
 		}
+    	
+    	int y = 0;
+    	System.out.println();
+    	
+    	if(compile_session) {
+    		System.out.println(GREEN + "Compile section detected !" + RESET);
+    		y += 1;
+    	}
+    	if(link_session) {
+    		System.out.println(GREEN + "Link section detected !" + RESET);
+    		y += 1;
+    	}
+    	if(run_session) {
+    		System.out.println(GREEN + "Run section detected !" + RESET);
+    		y += 1;
+    	}
+    	if(objects_session) {
+    		System.out.println(GREEN + "Object section detected !" + RESET);
+    		y += 1;
+    	}
+    	if(project_session) {
+    		System.out.println(GREEN + "Project section detected !" + RESET);
+    		y += 1;
+    	}
+    	
+    	if(y == 5) {
+    		System.out.println();
+    		System.out.println(GREEN + "All section are find, complete success !" + RESET);
+    	} else if(y < 5) {
+    		System.out.println();
+    		System.out.println(RED + "Less one section !" + RESET);
+    	} else if(y > 5) {
+    		System.out.println();
+    		System.out.println(RED + "Add one section !" + RESET);
+    	} else {
+    		System.out.println();
+    		System.out.println(RED + "Error !" + RESET);
+    	}
     }
     
     static boolean is_a_comment(String input) {
@@ -110,6 +161,8 @@ public class Main {
     }
     
     static void def_parts() {
+    	is_parts_cut = true;
+    	
     	boolean is_find = false;
     	
     	try(Scanner r = new Scanner(in)){
@@ -132,15 +185,12 @@ public class Main {
     					String type = o.strip();
     					
     					if(!compile_detected && type.equals("compile")) {
-    						System.out.println(GREEN + "Compile section detected !" + RESET);
     						compile_detected = true;
     						compile_session = false;
     					} else if(!link_detected && type.equals("link")) {
-    						System.out.println(GREEN + "Link section detected !" + RESET);
     						link_detected = true;
     						link_session = false;
     					} else if(!run_detected && type.equals("run")) {
-    						System.out.println(GREEN + "Run section detected !" + RESET);
     						run_detected = true;
     						run_session = false;
     					} else {
@@ -203,7 +253,7 @@ public class Main {
 						}
 					}
     				
-    				is_parts_cut = true;
+    				
     				
     			}
     		}
@@ -239,7 +289,37 @@ public class Main {
     	String[] objects_defs = all_objects.split(";");
     	
     	for(String ob : objects_defs) {
+    		String objet = ob.strip();
+    		String name = "";
+    		String path = "";
     		
+    		if(objet.isEmpty() || objet.equals("}")) continue;
+    		
+    		if(!is_a_comment(objet)) {
+        		if(objet.startsWith("c")) {
+        			name = objet.split(" ")[1];
+        			System.out.println(name);
+        			path = objet.substring(objet.indexOf("\'"), objet.lastIndexOf("\'"));
+        			
+        		}else if(objet.startsWith("rs")) {
+        			name = objet.split(" ")[1];
+        			System.out.println(name);
+        			path = objet.substring(objet.indexOf("\'"), objet.lastIndexOf("\'"));
+        			
+        		}else if(objet.startsWith("asm")) {
+        			name = objet.split(" ")[1];
+        			System.out.println(name);
+        			path = objet.substring(objet.indexOf("\'"), objet.lastIndexOf("\'"));
+        			
+        		}else if(objet.startsWith("ld")) {
+        			name = objet.split(" ")[1];
+        			System.out.println(name);
+        			path = objet.substring(objet.indexOf("\'"), objet.lastIndexOf("\'"));
+        			
+        		}else{
+            		System.out.println(RED + "Forge Error : Object type -" + RESET + YELLOW + objet.split(" ")[0] + RESET + RED + "- unknow" + RESET);
+        		}
+    		}
     	}
     	
     	obj_analised = true;
